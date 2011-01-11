59d66ba5e2bc3b5350ceb6a0c34cb4f08843200c
glennrune@gmail.com <glennrune@gmail.com@b02cdcb2-791e-8357-49d2-e27e3e28090b>
2011-01-11 15:04:02 +0000

    Added XML schema, XML template and the Race Bretonnia (with units) as  an XML file

--- WarhammerArmyBuilder/src/Warhammer/Unit.java
+++ WarhammerArmyBuilder/src/Warhammer/Unit.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2011 Glenn Rune
+ *  Copyright (C) 2011 Glenn Rune Strandbråten
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
