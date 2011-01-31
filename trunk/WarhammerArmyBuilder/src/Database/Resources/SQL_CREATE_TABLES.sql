27faa034e035e429adaf32b56babf88ccfe4c79d
glennrune@gmail.com <glennrune@gmail.com@b02cdcb2-791e-8357-49d2-e27e3e28090b>
2011-01-31 15:22:39 +0000

    Cleaned up the DatabaseManger, given it the possibility to connect/disconnect from the database without using hibernate and then post sql queries. Created more mapping files, some more work left, updated database again.

--- trunk/WarhammerArmyBuilder/src/Database/Resources/SQL_CREATE_TABLES.sql
+++ trunk/WarhammerArmyBuilder/src/Database/Resources/SQL_CREATE_TABLES.sql
@@ -1,7 +1,20 @@
+--Table to store every case.
+create table cases(ID int not null generated always as identity,
+ArmyPoints int,
+PlayerRace varchar(25),
+OpponentRace varchar(25),
+Outcome varchar(25),
+UNIQUE(ID));
 
+--Many-to-may relationship table between cases and units
+create table caseUnits(caseID int,
+unitID int,
+unitNumber int);
+
+--Table to store a unit represented in the casebase.
 create table Unit (ID int not null generated always as identity,
-name varchar(40),
-cost int not null,
+name varchar(40), 
+cost int, -- The base cost of the unit (not counting equipment, upgrades or mounts/crew.
 movement varchar(4),
 weaponSkill varchar(4),
 ballisticSkill varchar(4),
@@ -11,19 +24,34 @@ wounds varchar(4),
 initiative varchar(4),
 attack varchar(4),
 leadership varchar(4),
-unitType varchar(2),
-armyType varchar(7));
+unitType varchar(2), --unit type represents if this unit is classified as infantry, cavalry, monster etc.
+armyType varchar(7)); --army type represents if this unit is classified as core, rare, special, hero or lord
 
+--Table to store a crew or mount represented in the casebase.
+create table CrewMount(ID int not null generated always as identity,
+name varchar(40),
+cost int, -- The base cost of the crew/mount unit
+movement varchar(4),
+weaponSkill varchar(4),
+ballisticSkill varchar(4),
+strength varchar(4),
+toughness varchar(4),
+wounds varchar(4),
+initiative varchar(4),
+attack varchar(4),
+leadership varchar(4),
+unitType varchar(2), --unit type represents if this crew/mount is classified as infantry, cavalry, monster etc.
+armyType varchar(7)); --army type represents if this crew/mount is classified as core, rare, special, hero or lord
+
+--Many-to-many relationship table between units and crews/mounts
+create table unitCrew(unitID int , crewID int);
+
+--Table to store the equipment represented in the casebase
 create table equipment (ID int generated always as identity,
 cost int,
 name varchar(30),
 type varchar(15));
 
+--Many-to-many relationshipt table betwen units and equipment.
 create table unitEquipment(unitID int, equipmentID int);
 
-create table cases(ID int not null generated always as identity,
-ArmyPoints int,
-race varchar(20),
-victory varchar(20));
-
-create table caseUnits(caseID int, unitID int);
---
+++ trunk/WarhammerArmyBuilder/src/Warhammer/Case.java
@@ -0,0 +1,140 @@
+/*
+ *  Copyright (C) 2011 Glenn Rune Strandbråten 
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package Warhammer;
+
+import java.util.Set;
+import jcolibri.cbrcore.Attribute;
+
+/**
+ *
+ * @author Glenn Rune Strandbråten
+ * @version 
+ */
+public class Case implements jcolibri.cbrcore.CaseComponent{
+
+    private Set<UnitCase> units;
+    private Set<Integer> count;
+    private int armyPoints;
+    private int ID;
+    private String playerRace;
+    private String opponentRace;
+    private String outcome;
+
+    /**
+     * @return the units
+     */
+    public Set<UnitCase> getUnits() {
+        return units;
+    }
+
+    /**
+     * @param units the units to set
+     */
+    public void setUnits(Set<UnitCase> units) {
+        this.units = units;
+    }
+
+    /**
+     * @return the count
+     */
+    public Set<Integer> getCount() {
+        return count;
+    }
+
+    /**
+     * @param count the count to set
+     */
+    public void setCount(Set<Integer> count) {
+        this.count = count;
+    }
+
+    /**
+     * @return the armyPoints
+     */
+    public int getArmyPoints() {
+        return armyPoints;
+    }
+
+    /**
+     * @param armyPoints the armyPoints to set
+     */
+    public void setArmyPoints(int armyPoints) {
+        this.armyPoints = armyPoints;
+    }
+
+    /**
+     * @return the playerRace
+     */
+    public String getPlayerRace() {
+        return playerRace;
+    }
+
+    /**
+     * @param playerRace the playerRace to set
+     */
+    public void setPlayerRace(String playerRace) {
+        this.playerRace = playerRace;
+    }
+
+    /**
+     * @return the opponentRace
+     */
+    public String getOpponentRace() {
+        return opponentRace;
+    }
+
+    /**
+     * @param opponentRace the opponentRace to set
+     */
+    public void setOpponentRace(String opponentRace) {
+        this.opponentRace = opponentRace;
+    }
+
+    /**
+     * @return the outcome
+     */
+    public String getOutcome() {
+        return outcome;
+    }
+
+    /**
+     * @param outcome the outcome to set
+     */
+    public void setOutcome(String outcome) {
+        this.outcome = outcome;
+    }
+
+    public Attribute getIdAttribute() {
+        return new Attribute("ID", this.getClass());
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+}
